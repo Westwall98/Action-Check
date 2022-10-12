94037a966a42594db51840102c9727a6027fa02e
Westwall98 <35027934+Westwall98@users.noreply.github.com>
2022-10-12 14:10:20 +0800

    Update role.py

--- Python/role.py
+++ Python/role.py
@@ -44,7 +44,7 @@ def run(roomlistdict):
 
 def run3F(roomlistdict):
 	
-	driver = webdriver.Chrome(executable_path=chromedriver)
+	driver = webdriver.Chrome(chromedriver)
 	driver.get(os.getenv('FORM'))
 	sleep(1)
 
@@ -69,7 +69,7 @@ def run3F(roomlistdict):
 
 def run5F(roomlistdict):
 	
-	driver = webdriver.Chrome(executable_path=chromedriver)
+	driver = webdriver.Chrome(chromedriver)
 	driver.get(os.getenv('FORM'))
 	sleep(1)
 
@@ -94,7 +94,7 @@ def run5F(roomlistdict):
 
 def run6F(roomlistdict):
 	
-	driver = webdriver.Chrome(executable_path=chromedriver)
+	driver = webdriver.Chrome(chromedriver)
 	driver.get(os.getenv('FORM'))
 	sleep(1)
 
@@ -119,7 +119,7 @@ def run6F(roomlistdict):
 
 def run7F(roomlistdict):
 	
-	driver = webdriver.Chrome(executable_path=chromedriver)
+	driver = webdriver.Chrome(chromedriver)
 	driver.get(os.getenv('FORM'))
 	sleep(1)
 
@@ -144,7 +144,7 @@ def run7F(roomlistdict):
 
 def runDaily(roomlistdict):
 	
-	driver = webdriver.Chrome(executable_path=chromedriver)
+	driver = webdriver.Chrome(chromedriver)
 	driver.get(os.getenv('FORM'))
 	sleep(1)
 
