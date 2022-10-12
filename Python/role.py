c03a1b6e242d0ab7639bb1b76192b0a4733af768
Westwall98 <35027934+Westwall98@users.noreply.github.com>
2022-10-12 13:01:28 +0800

    Update role.py

--- Python/role.py
+++ Python/role.py
@@ -1,34 +1,46 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
 from time import sleep
 from datetime import date
 import datetime
 import requests
 import os
 
-# For Chrome 105 #
+roomlistdict = {
+0:{30:("3F","Training Room"),50:("5F","Absolut"),60:("6F","Martell Meal"),61:("6F","Chivas"),70:("7F","Boardroom")},
+3:{31:"Altos",32:"Havana Club",33:"Kinobi",34:"Jacob's Creek",35:"Lillet",36:"Ballantine's Finest"},
+5:{51:"Aberlour",52:"Church Road",53:"Kahlúa",54:"Ricard",55:"Beefeater",56:"Olmeca",57:"Monkey 47",58:"The Glenlivet"},
+6:{62:"Drinks & Co",63:"Malibu",64:"Martell PTI",65:"Mumm",66:"Pernod",67:"Secret Speyside",68:"Perrier-Jouet",69:"Royal Salute"},
+7:{71:"Cognac",72:"Jameson",73:"Whisky",74:"White spirits",75:"Wine",76:"Martell QSS"}
+}
 
-F5roomlist = ['Aberlour','Church Road','Kahlúa','Ricard','Beefeater','Olmeca','Monkey 47','The Glenlivet']
-F7roomlist = ['Cognac','Jameson','Whisky','White spirits','Wine','Martell QSS']
-Dailyroomlist = [['7F','Boardroom']]
-
-def run(F5roomlist,F7roomlist,Dailyroomlist):
-
+def run(roomlistdict):
 	weekday = datetime.date.today().isoweekday()
 	apiurl = os.getenv('URL')
 	apidate = datetime.date.today()
 	payload={'text': '{}会议室巡检运行成功'.format(apidate)}
 
 	if weekday == 1 or weekday == 3:
-		run7F(F7roomlist)
+		print("将执行以下自动化：\n" + str(roomlistdict[0]) + '\n' + str(roomlistdict[6]) + '\n' + str(roomlistdict[7]))
+		run6F(roomlistdict)
+		run7F(roomlistdict)
+		runDaily(roomlistdict)
+		print('6F&7F Success')
 	elif weekday == 2 or weekday == 4:
-		run5F(F5roomlist)
+		print("将执行以下自动化：\n" + str(roomlistdict[0]) + '\n' + str(roomlistdict[3]) + '\n' + str(roomlistdict[5]))
+		run3F(roomlistdict)
+		run5F(roomlistdict)
+		runDaily(roomlistdict)
+		print('3F&5F Success')
 	elif weekday == 5:
-		runDaily(Dailyroomlist)
+		print("将执行以下自动化：\n" + str(roomlistdict[0]))
+		runDaily(roomlistdict)
+		print('Daily Success')
+	
 	response = requests.post(apiurl, data = payload)
+	
 
-def run5F(F5roomlist):
+def run3F(roomlistdict):
 	
 	chrome_options = webdriver.ChromeOptions()
 	chrome_options.add_argument('--headless')
@@ -41,7 +53,39 @@ def run5F(F5roomlist):
 	driver.get("https://forms.office.com/r/3AQwbrGbms")
 	sleep(2)
 
-	for meetingroom5 in F5roomlist:
+	for meetingroom3 in roomlistdict[3].values:
+		driver.find_element(By.XPATH,value="//div[@aria-posinset='0']").click()
+		sleep(0.5)
+		driver.find_element(By.XPATH,value="//div[@aria-label='3F']").click()
+		sleep(0.5)
+		driver.find_element(By.XPATH,value="//div[@aria-posinset='0']").click()
+		sleep(0.5)
+		driver.find_element(By.XPATH,value="//div[@aria-label=\"{}\"]".format(str(meetingroom3))).click()
+		sleep(0.5)
+		driver.find_element(By.XPATH,value="//div[@aria-posinset='0']").click()
+		sleep(0.5)
+		driver.find_element(By.XPATH,value="//div[@aria-label='Good']").click()
+		sleep(0.5)
+		driver.find_element(By.XPATH,value="//button[@title='Submit']").click()
+		sleep(2)
+		driver.find_element(By.LINK_TEXT,value="Submit another response").click()
+		sleep(0.5)
+	driver.close()
+
+def run5F(roomlistdict):
+
+	chrome_options = webdriver.ChromeOptions()
+	chrome_options.add_argument('--headless')
+	chrome_options.add_argument('--no-sandbox')
+	chrome_options.add_argument('--disable-gpu')
+	chrome_options.add_argument('--disable-dev-shm-usage')
+	chromedriver = "/usr/bin/chromedriver"
+	os.environ["webdriver.chrome.driver"] = chromedriver
+	driver = webdriver.Chrome(chrome_options=chrome_options,executable_path=chromedriver)
+	driver.get("https://forms.office.com/r/3AQwbrGbms")
+	sleep(2)
+
+	for meetingroom5 in roomlistdict[5].values:
 		driver.find_element(By.XPATH,value="//div[@aria-posinset='0']").click()
 		sleep(0.5)
 		driver.find_element(By.XPATH,value="//div[@aria-label='5F']").click()
@@ -53,14 +97,14 @@ def run5F(F5roomlist):
 		driver.find_element(By.XPATH,value="//div[@aria-posinset='0']").click()
 		sleep(0.5)
 		driver.find_element(By.XPATH,value="//div[@aria-label='Good']").click()
-		sleep(1)
+		sleep(0.5)
 		driver.find_element(By.XPATH,value="//button[@title='Submit']").click()
-		sleep(3)
+		sleep(2)
 		driver.find_element(By.LINK_TEXT,value="Submit another response").click()
 		sleep(0.5)
 	driver.close()
 
-def run7F(F7roomlist):
+def run6F(roomlistdict):
 	
 	chrome_options = webdriver.ChromeOptions()
 	chrome_options.add_argument('--headless')
@@ -70,11 +114,42 @@ def run7F(F7roomlist):
 	chromedriver = "/usr/bin/chromedriver"
 	os.environ["webdriver.chrome.driver"] = chromedriver
 	driver = webdriver.Chrome(chrome_options=chrome_options,executable_path=chromedriver)
-
 	driver.get("https://forms.office.com/r/3AQwbrGbms")
 	sleep(2)
 
-	for meetingroom7 in F7roomlist:
+	for meetingroom6 in roomlistdict[6].values:
+		driver.find_element(By.XPATH,value="//div[@aria-posinset='0']").click()
+		sleep(0.5)
+		driver.find_element(By.XPATH,value="//div[@aria-label='6F']").click()
+		sleep(0.5)
+		driver.find_element(By.XPATH,value="//div[@aria-posinset='0']").click()
+		sleep(0.5)
+		driver.find_element(By.XPATH,value="//div[@aria-label=\"{}\"]".format(str(meetingroom6))).click()
+		sleep(0.5)
+		driver.find_element(By.XPATH,value="//div[@aria-posinset='0']").click()
+		sleep(0.5)
+		driver.find_element(By.XPATH,value="//div[@aria-label='Good']").click()
+		sleep(0.5)
+		driver.find_element(By.XPATH,value="//button[@title='Submit']").click()
+		sleep(2)
+		driver.find_element(By.LINK_TEXT,value="Submit another response").click()
+		sleep(0.5)
+	driver.close()
+
+def run7F(roomlistdict):
+	
+	chrome_options = webdriver.ChromeOptions()
+	chrome_options.add_argument('--headless')
+	chrome_options.add_argument('--no-sandbox')
+	chrome_options.add_argument('--disable-gpu')
+	chrome_options.add_argument('--disable-dev-shm-usage')
+	chromedriver = "/usr/bin/chromedriver"
+	os.environ["webdriver.chrome.driver"] = chromedriver
+	driver = webdriver.Chrome(chrome_options=chrome_options,executable_path=chromedriver)
+	driver.get("https://forms.office.com/r/3AQwbrGbms")
+	sleep(2)
+
+	for meetingroom7 in roomlistdict[7].values:
 		driver.find_element(By.XPATH,value="//div[@aria-posinset='0']").click()
 		sleep(0.5)
 		driver.find_element(By.XPATH,value="//div[@aria-label='7F']").click()
@@ -86,14 +161,14 @@ def run7F(F7roomlist):
 		driver.find_element(By.XPATH,value="//div[@aria-posinset='0']").click()
 		sleep(0.5)
 		driver.find_element(By.XPATH,value="//div[@aria-label='Good']").click()
-		sleep(1)
+		sleep(0.5)
 		driver.find_element(By.XPATH,value="//button[@title='Submit']").click()
-		sleep(3)
+		sleep(2)
 		driver.find_element(By.LINK_TEXT,value="Submit another response").click()
 		sleep(0.5)
 	driver.close()
 
-def runDaily(Dailyroomlist):
+def runDaily(roomlistdict):
 	
 	chrome_options = webdriver.ChromeOptions()
 	chrome_options.add_argument('--headless')
@@ -106,7 +181,7 @@ def runDaily(Dailyroomlist):
 	driver.get("https://forms.office.com/r/3AQwbrGbms")
 	sleep(2)
 
-	for Dailycheckresult in Dailyroomlist:
+	for Dailycheckresult in roomlistdict[0].values():
 		driver.find_element(By.XPATH,value="//div[@aria-posinset='0']").click()
 		sleep(0.5)
 		driver.find_element(By.XPATH,value="//div[@aria-label=\"{}\"]".format(str(Dailycheckresult[0]))).click()
@@ -118,11 +193,11 @@ def runDaily(Dailyroomlist):
 		driver.find_element(By.XPATH,value="//div[@aria-posinset='0']").click()
 		sleep(0.5)
 		driver.find_element(By.XPATH,value="//div[@aria-label='Good']").click()
-		sleep(1)
+		sleep(0.5)
 		driver.find_element(By.XPATH,value="//button[@title='Submit']").click()
-		sleep(3)
+		sleep(2)
 		driver.find_element(By.LINK_TEXT,value="Submit another response").click()
 		sleep(0.5)
 	driver.close()
 
-run(F5roomlist,F7roomlist,Dailyroomlist)
+run(roomlistdict)
